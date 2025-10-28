73dc4a74bb640cce8727161b366584e7c17a1959
thomas550i <thomas550i@gmail.com>
2025-10-28 15:33:04 +0530

    status api

--- frontend/src/components/Kanban/KanbanView.vue
+++ frontend/src/components/Kanban/KanbanView.vue
@@ -191,8 +191,10 @@ async function insertLinkedStatusIfNeeded(newStatus) {
     const linkedDoctype = meta.options;
     console.log('Calling frappe.client.insert for:', newStatus, 'in', linkedDoctype);
       await call('frappe.client.insert', {
+        doc: {
           doctype: linkedDoctype,
-          status: newStatus
+          Status: newStatus
+        }
       });
   } else {
     console.log('Not a Link field or missing meta:', meta);
